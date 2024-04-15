a779bdae3c1ec8e76ab9a07b72592b7646d5f68b
dredegui <gui.pascoal@outlook.com>
2024-04-15 22:35:50 +0100

    feat: add footer to app.jsx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,3 +1,16 @@
+const App = () => {
+  const name = 'Peter'
+  const age = 10  
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26+10}/>
+      <Hello name={name} age={age}/>
+      <Footer />
+    </div>
+  )
+}
+
 const Hello = (props) => {
   console.log(props)
   return (
@@ -7,14 +20,10 @@ const Hello = (props) => {
   )
 }
 
-const App = () => {
-  const name = 'Peter'
-  const age = 10  
+const Footer = () => {
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name='Maya' age={26+10}/>
-      <Hello name={name} age={age}/>
+      greeting app created by <a href="https://github.com/Dredegui">Dredegui</a>
     </div>
   )
 }
